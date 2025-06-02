5fec02afa6bc066f5851aee711b0196e068e8d5e
newmin9999 <mihnhyuk@naver.com>
2025-06-02 14:19:10 +0900

    dev cors 추가

--- src/main/java/com/supience/config/DevCorsConfig.java
+++ src/main/java/com/supience/config/DevCorsConfig.java
@@ -18,7 +18,9 @@ public class DevCorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
             "http://localhost:3000",
-            "http://127.0.0.1:3000"
+            "http://localhost:3001",
+            "http://127.0.0.1:3000",
+            "http://127.0.0.1:3001"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
